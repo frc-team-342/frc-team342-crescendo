7b0ff9384515007989995895b1ecb2aabfc6ceae
Cadence69 <cadence.sloan@gmail.com>
2024-01-13 14:09:29 -0500

    intake subsystem, declaring motors and methods

---
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,49 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Intake extends SubsystemBase {
+
+  private final CANSparkMax intake;
+  private final CANSparkMax wrist;
+  private final CANSparkMax elevator;
+
+
+  /** Creates a new Intake. */
+  public Intake() {
+    intake = new CANSparkMax(0, MotorType.kBrushless);
+    wrist = new CANSparkMax(0, MotorType.kBrushless);
+    elevator = new CANSparkMax(0, MotorType.kBrushless);
+
+  }
+
+
+  public void spinIntake(double speed){
+    intake.set(speed);
+  }
+
+  public void rotateWrist(double angle){
+    wrist.set(50%);
+  }
+
+  public void raiseElevatorwithSpeed(double speed){
+    elevator.set(speed);
+  }
+
+  public void raiseElevatorwithPosition(double pos){
+    elevator.set(pos);
+  }
+
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
