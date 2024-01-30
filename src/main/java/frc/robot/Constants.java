78cfe93d9e460ad21acd0e6546b6bf074a8181db
Cadence69 <cadence.sloan@gmail.com>
2024-01-30 18:51:04 -0500

    when sensor 1 is triggered the motor will spin. working on intake.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -13,6 +13,9 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
+
+  public static final int INTAKE_SENSOR_1 = 1;
+
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
   }
--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -4,12 +4,15 @@
 
 package frc.robot.subsystems;
 
+import frc.robot.Constants;
+
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.SparkPIDController;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.DigitalInput;
 
 
 
@@ -19,6 +22,7 @@ public class Intake extends SubsystemBase {
   private final CANSparkMax wrist;
   private final CANSparkMax elevator;
   private final SparkPIDController pid_elevator;
+  private DigitalInput sensor1;
 
 
   /** Creates a new Intake. */
@@ -27,11 +31,26 @@ public class Intake extends SubsystemBase {
     wrist = new CANSparkMax(2, MotorType.kBrushless);
     elevator = new CANSparkMax(3, MotorType.kBrushless);
     pid_elevator = elevator.getPIDController();
+
+    sensor1 = new DigitalInput(Constants.INTAKE_SENSOR_1);
   }
 
 
   public Command spinIntake(double speed){
-    return runEnd( () -> {intake.set(speed);}, () -> {intake.set(0);});
+    return runEnd( () -> {
+      if(!sensor1.get()){
+
+      intake.set(speed);
+
+      }
+      else {
+
+        intake.set(0);
+        
+      }
+    }, 
+    
+    () -> {intake.set(0);});
 
 
   }
